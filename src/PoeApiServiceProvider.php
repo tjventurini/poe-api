cc503bf3c44e11943059122cc631dcb1ad56c2e5
Thomas Venturini <thomas@venturini.codes>
2020-02-29 22:58:59 +0100

    initial commit

---
+++ src/PoeApiServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tjventurini\PoeApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class PoeApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot method of this service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register method of this service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
